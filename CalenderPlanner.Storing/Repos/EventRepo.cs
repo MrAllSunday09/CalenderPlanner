409d4976904b8f58ea34d9f50a615e3c646cf47a
daniel henderson <drayhender@gmail.com>
2021-05-19 21:40:53 -0400

    pull from org

--- CalenderPlanner.Storing/Repos/EventRepo.cs
+++ CalenderPlanner.Storing/Repos/EventRepo.cs
@@ -1,7 +1,38 @@
+using System;
+using System.Collections.Generic;
+using CalenderPlanner.Domain.Interfaces;
+using CalenderPlanner.Domain.Models;
+
 namespace CalenderPlanner.Storing.Repos
 {
-    public class EventRepo : IRepo<Event>
+  public class EventRepo : IRepo<Event>
+  {
+    private readonly CPContext _context;
+
+    public EventRepo(CPContext context)
     {
-        
+      _context = context;
     }
+
+    public IEnumerable<Event> Create(Func<Event, bool> filter)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public bool Read(Event entry)
+    {
+      _context.Events.Add(entry);
+      return true;
+    }
+
+    public Event Update()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public bool Delete()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
 }
